bcb9f8cd46d7986a6c35dcfd4894187ff39dd2fe
mark-christiansen <m_christiansen@comcast.net>
2023-02-03 19:26:31 -0600

    fixed streams app to not stop right away

--- streams/src/main/java/com/mycompany/kafka/streams/Application.java
+++ streams/src/main/java/com/mycompany/kafka/streams/Application.java
@@ -4,18 +4,28 @@ import com.mycompany.kafka.common.streams.StreamsLifecycle;
 
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.runtime.Micronaut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
 
 public class Application {
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         try (ApplicationContext ctx = Micronaut.run(Application.class, args)) {
+
             Optional<StreamsLifecycle> bean = ctx.findBean(StreamsLifecycle.class);
             if (bean.isPresent()) {
-                bean.get().start();
+
+                StreamsLifecycle lifecycle = bean.get();
+                lifecycle.start();
+                Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                    log.info("Application shutdown event called");
+                    lifecycle.stop();
+                }));
             }
         }
-        System.exit(0);
     }
 }
